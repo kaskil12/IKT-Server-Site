6025d8840e68fd155e2a8e027999bf631b64012a
Kasper Kilde <kasperkilde2@gmail.com>
2024-10-30 11:29:48 +0100

    added header but still working on it

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
-
+import Header from './Components/Header.tsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header></Header>
     </>
   )
 }
---
+++ react/src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Header() {
+    return (
+        <>
+            <header className="bg-blue-500 p-4">
+                <h1 className="text-white text-center">IKT</h1>
+            </header>
+        </>
+    );
+}
