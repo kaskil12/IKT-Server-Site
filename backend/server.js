c4caac6fe18a4170db0bcbd8c3f10222ca796dd1
Kasper Kilde <kasperkilde2@gmail.com>
2024-11-08 10:29:36 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors'); 
 const app = express();
 const port = 3000;
 const sequelizeDB = require("./database.js");
@@ -8,20 +8,20 @@ const Liste = require("./models/Liste.js");
 Liste.init(sequelizeDB);
 Liste.sync({force: true});
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/getAll', async (req, res) => {
   const data = await Liste.findAll({
-    order: [['dato', 'ASC']]  // Sort by the date (dato)
+    order: [['dato', 'ASC']] 
   });
   res.send(data);
 });
 
 app.post('/add', async (req, res) => {
   const { dato, antall } = req.body;
-  console.log('Received data:', { dato, antall }); // Log received data
+  console.log('Received data:', { dato, antall }); 
 
   // Validate input
   if (!dato || !antall) {
